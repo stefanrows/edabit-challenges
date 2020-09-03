42b98064a4409a65c2be3788a20f03d3db152e09
Stefan Rows <38349705+stefanrows@users.noreply.github.com>
2020-09-03 09:24:03 +0200

    Create count-instances.js

---
+++ Easy/count-instances.js
@@ -0,0 +1,9 @@
+// Create a function that takes two strings as arguments and returns the number of times the first string (the single character) is found in the second string.
+
+function charCount(myChar, str) {
+	let counter = 0;
+	let arr = str.split(' ');
+	console.log(arr);
+}
+
+charCount('a', 'The brown fox is a bitch and fucks a lot');
